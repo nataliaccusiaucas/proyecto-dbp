4789a1b6a32810329aec089678a65b2e3d48e511
NataliaCcusi <lucero.cat.9@hotmail.com>
2025-11-25 23:02:48 -0500

    35

--- src/main/java/com/hirehub/backend/payment/service/PaymentService.java
+++ src/main/java/com/hirehub/backend/payment/service/PaymentService.java
@@ -1,40 +1,40 @@
-package com.hirehub.backend.payment.service;
+//package com.hirehub.backend.payment.service;
 
-import com.culqi.api.Charge;
-import com.culqi.model.ChargeModel;
-import com.hirehub.backend.payment.dto.CreateChargeRequestDTO;
-import com.hirehub.backend.payment.dto.CreateChargeResponseDTO;
-import org.springframework.stereotype.Service;
+//import com.culqi.api.Charge;
+//import com.culqi.model.ChargeModel;
+//import com.hirehub.backend.payment.dto.CreateChargeRequestDTO;
+//import com.hirehub.backend.payment.dto.CreateChargeResponseDTO;
+//import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.Map;
+//import java.util.HashMap;
+//import java.util.Map;
 
-@Service
-public class PaymentService {
+//@Service
+//public class PaymentService {
 
-    public CreateChargeResponseDTO createCharge(CreateChargeRequestDTO req) {
-        try {
-            Charge charge = new Charge();
+//   public CreateChargeResponseDTO createCharge(CreateChargeRequestDTO req) {
+//       try {
+//            Charge charge = new Charge();
 
-            Map<String, Object> map = new HashMap<>();
-            map.put("amount", req.amount);
-            map.put("currency_code", "PEN");
-            map.put("email", req.email);
-            map.put("source_id", req.sourceId);
+//            Map<String, Object> map = new HashMap<>();
+//           map.put("amount", req.amount);
+//            map.put("currency_code", "PEN");
+//           map.put("email", req.email);
+//        map.put("source_id", req.sourceId);
 
-            ChargeModel chargeResult = charge.create(map);
+//     ChargeModel chargeResult = charge.create(map);
 
-            return new CreateChargeResponseDTO(
-                    true,
-                    chargeResult.getId(),
-                    "Pago realizado correctamente."
-            );
-        } catch (Exception e) {
-            return new CreateChargeResponseDTO(
-                    false,
-                    null,
-                    "Error al procesar el pago: " + e.getMessage()
-            );
-        }
-    }
-}
+//         return new CreateChargeResponseDTO(
+//                   true,
+//                  chargeResult.getId(),
+//                   "Pago realizado correctamente."
+//     );
+//  } catch (Exception e) {
+//      return new CreateChargeResponseDTO(
+//            false,
+//               null,
+//                 "Error al procesar el pago: " + e.getMessage()
+//         );
+                    //   }
+//  }
+//}
